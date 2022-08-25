67f3739731760adefb503cf90f9c44776f1b30b8
Jimi-Chhatrala <90768094+Jimi-Chhatrala@users.noreply.github.com>
2022-08-25 18:01:20 +0530

    variables and values

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,27 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
 
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jimi");
+console.log(23);
+
+let firstName = "Jimikumar";
+console.log(firstName);
+let $function = 27;
+let name = "Jims"; // never use name as variable name just fact
+let Person = "Jimit"; //not recommended
+let person = "jimii";
+let PI = 3.14;
+let PERSON = 12;
+// allowed
+// a,A,$,_ for variables to start name with
+// 2 allowed only bewtween or last for variables names
+
+let myFirstJob = "Programmer"; // descriptive variable names
+let myCurrentJob = "Coach"; // descriptive variable names
+
+let job1 = "programmer"; // descriptive variable names not recomment
+
+let job2 = "coach";
+
+console.log(myFirstJob);
