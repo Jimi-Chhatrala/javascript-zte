b3a627aef22f19fdfa7710338d4052e0b4966d8d
jimi-chhatrala <jimichhatrala8488@gmail.com>
2022-10-03 19:18:45 +0530

    Ternary Conditional Operator

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -466,16 +466,44 @@ if (day === "monday") {
  */
 
 // Statements
-
+/*
 if (10 > 5) {
   const bigNo = "10 is bigger";
 }
-
+*/
 /**
  * ${2030 - 1990}
  * ${myName}
  */
-
+/*
 const myName = "Jimi";
 
 console.log(`I'm ${2030 - 1990} yeard old ${myName}.`);
+*/
+
+// *******************************************************
+
+// Ternary Conditional Operator
+
+// *******************************************************
+
+const age = 15;
+
+age >= 18
+  ? console.log("I like to drink wine.. 🍷")
+  : console.log("I like to drink water.. 💧");
+
+const drink = age >= 18 ? "wine 🍷" : "water 💧";
+console.log(drink);
+
+let drink2;
+
+if (age >= 18) {
+  drink2 = "wine 🍷";
+} else {
+  drink2 = "water 💧";
+}
+
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
