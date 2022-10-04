262aae293b245e1d9568e950b13c8814c4632a98
jimi-chhatrala <jimichhatrala8488@gmail.com>
2022-10-04 22:51:43 +0530

    Functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+// ************************************************
+
+// use strict mode
+
+// ************************************************
+/*
+"use strict";
+
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -10,3 +18,40 @@ if (hasDriversLicense) console.log("I can drive :)");
 
 // const interface = "Audio";
 // const private = 123;
+
+*/
+
+// ************************************************
+
+// Functions
+
+// ************************************************
+
+function logger() {
+  console.log("My name is Jimi.");
+}
+
+// calling, running, invoking function
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+// console.log(fruitProcessor(3, 2));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("123");
+console.log(num);
