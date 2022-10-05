7128acd845b46710ae284d225ff6fcba32cf4155
jimi-chhatrala <jimichhatrala8488@gmail.com>
2022-10-05 15:04:38 +0530

    return KEYWORD EXITS FUNCTION

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -115,7 +115,7 @@ console.log(yearsUntilRetirement(2000, "Jimi"));
 // Functions Calling Other Functions
 
 // *******************************************************
-
+/*
 function cutFruitPieces(fruit) {
   return fruit * 4;
 }
@@ -130,3 +130,33 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(3, 4));
+*/
+
+// ******************************************************
+
+// Return Keyword Exits From Function
+
+// ******************************************************
+/*
+const calcAge = function (birthYear) {
+  return 2022 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years.`);
+    return retirement;
+    console.log(`'return' KEYWORD EXITS WORKING OF FUNCTION`);
+  } else {
+    console.log(`${firstName} has already retired..!`);
+    return -1;
+    console.log(`'return' KEYWORD EXITS WORKING OF FUNCTION`);
+  }
+};
+
+console.log(yearsUntilRetirement(2000, "Jimi"));
+console.log(yearsUntilRetirement(1955, "Ramkumar"));
+*/
