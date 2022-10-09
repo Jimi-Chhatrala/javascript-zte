624593d66abe0f41ea2fc0bfb6373dd49deec99a
jimi-chhatrala <jimichhatrala8488@gmail.com>
2022-10-09 22:23:27 +0530

    Array Problem Statement

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -250,7 +250,7 @@ console.log(ages);
 // Array Methods
 
 // *************************************************
-
+/*
 const friends = ["Ram", "Laksman", "Bharat"];
 console.log(friends);
 
@@ -293,3 +293,24 @@ console.log(friends.includes(24)); // For number also
 if (friends.includes("Jimi")) {
   console.log("Hey, Jimi is there in array.");
 }
+*/
+
+// *******************************************************
+
+// Problem Statement
+
+// *******************************************************
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+// const calcTipNewVersion =  (bill) => bill >= 50 && bill <= 300 ? bill * 0.15 : bill - 0.2;
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
