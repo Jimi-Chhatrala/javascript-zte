9d7c969a51d383844a87c45acd47923365a18b56
jimi-chhatrala <jimichhatrala8488@gmail.com>
2022-12-02 09:40:45 +0530

    Temperature Analyse Problem Statement

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,2 +1,63 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+
+//     if (typeof curTemp != 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log('max: ', max);
+//   console.log('min: ', min);
+//   return max - min;
+// };
+
+// // calcTempAmplitude([3, 6, 8, 5, 1]);
+// const amplitude = calcTempAmplitude(temperatures);
+
+// console.log('amplitude: ', amplitude);
+
+// *********************
+// Merge 2 Arrays
+// *********************
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// const array1 = ['a', 'b', 'c'];
+// const array2 = ['d', 'e', 'f'];
+// const array3 = array1.concat(array2);
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp != 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log('max: ', max);
+  console.log('min: ', min);
+  return max - min;
+};
+
+// calcTempAmplitude([3, 6, 8, 5, 1]);
+// const amplitudeNew = calcTempAmplitudeNew(temperatures);
+
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 2, 0]);
+
+console.log('amplitude: ', amplitudeNew);
